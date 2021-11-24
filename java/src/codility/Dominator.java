cdfdde049d0adcb80e6424452bda52054ea16722
Mario Neises <git@mnio.net>
2021-11-24 08:51:57 +0100

    new dominator impl

--- java/src/codility/Dominator.java
+++ java/src/codility/Dominator.java
@@ -8,14 +8,52 @@ public class Dominator {
     public static record CountPos(AtomicInteger count, List<Integer> positions) {
     }
 
-    ;
-
     public static void main(String[] args) {
         int[] solution = solution(new int[]{3, 4, 3, 2, 3, -1, 3, 3});
         assert Arrays.equals(solution, new int[]{0, 2, 4, 6, 7});
     }
 
-    private static int[] solution(int[] a) {
+    private static int[] solution(int[] ints) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(ints[0]);
+
+        for (int i : ints) {
+            if (stack.isEmpty()) {
+                stack.push(i);
+            } else {
+                final Integer peek = stack.peek();
+                if (peek == null) {
+                    throw new IllegalStateException("should not happen");
+                }
+                if (peek == i) {
+                    stack.push(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return new int[]{-1};
+        }
+
+        int dominator = stack.peek();
+        int[] positions = new int[ints.length];
+        Arrays.fill(positions, -1);
+        int currentPosition = 0;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == dominator) {
+                positions[currentPosition] = i;
+                currentPosition++;
+            }
+        }
+
+
+        return Arrays.copyOf(positions, currentPosition);
+    }
+
+
+    private static int[] solution2(int[] a) {
         // map: Value -> {Count, Pos[]}
         final Map<Integer, CountPos> map = new HashMap<>();
         int maxCount = -1;
